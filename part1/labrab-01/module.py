39a80ad18c6689de8d139bab0929041b01cf05f7
Андрей Беляков <ttxiom@gmail.com>
2021-09-20 18:09:25 +0500

    up

---
+++ part1/labrab-01/module.py
@@ -0,0 +1,27 @@
+def isEven(value):
+    '''проверить на чётность'''
+    return "чётное" if value % 2 == 0 else "нечетное"
+
+
+def isDivide(x, y):
+  if x > y:
+    return x % y == 0
+  else:
+    return y % x == 0
+ 
+
+
+
+'''
+
+def SortMaxMin(a, b): 
+    return a,b if a>b else b,a
+
+
+
+
+
+
+
+'''
+
