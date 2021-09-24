d85f7b2ff5d3d8bdcf02e9057d0ada7e22a3ef94
Андрей Беляков <ttxiom@gmail.com>
2021-09-24 11:37:52 +0500

    Update 01.py

--- part1/labrab03/01.py
+++ part1/labrab03/01.py
@@ -1,16 +1,16 @@
 def play(maxCount=3, xLeft=10, xRight=99):
-    count = 1
+    count, x = 1, 0
     while True:
         x = int(input(f"Попытка {count}. Введите число [{xLeft},{xRight}] - "))
         if count >= maxCount or xLeft <= x <= xRight:
             break
         count += 1
-    return -1 if count > maxCount else x
+    return (count < maxCount, x)
 
 
 maxCount, xLeft, xRight = 3, 10, 99
-num = play()
-if num < 0:
-    print("Повтор попыток через 60 сек.")
-else:
+check, num = play()
+if check:
     print(num)
+else:
+    print("Повтор попыток через 60 сек.")
