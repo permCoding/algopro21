58200f77bd4770832a89f9c2905234641285c145
Андрей Беляков <ttxiom@gmail.com>
2021-10-01 10:51:58 +0500

    up

---
+++ part1/LabRab/labrab-04/game.py
@@ -0,0 +1,14 @@
+from random import randint as rint
+
+
+x = rint(100, 999)
+money = 1000
+
+while money > 0:
+    # диалог с пользователем
+    # >   <
+    if True:  # если == то угадал и выходим
+        break
+    
+print(x)  # сколько денег удалось выиграть
+
