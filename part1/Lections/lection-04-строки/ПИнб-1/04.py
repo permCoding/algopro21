69db8aaa14d30e87b2540ff376fed17283b34b3a
Андрей Беляков <ttxiom@gmail.com>
2021-10-11 16:13:46 +0500

    Update 04.py

--- part1/Lections/lection-04-строки/ПИнб-1/04.py
+++ part1/Lections/lection-04-строки/ПИнб-1/04.py
@@ -1,21 +1,43 @@
 def get_list(s):
+    '''
+    если последний символ не пробел
+    то функция не работает
+    '''
     lst = []
     line = ''
     for smb in s:
-        if smb != ' ':
-            line += smb
-        else:
+        if smb == ' ':
             lst.append(line)
             line = ''
+        else:
+            line += smb
     return lst
+
+
+def revers_list(lst):
+    new_lst = []
+    i = len(lst) - 1 
+    while i >= 0:
+        new_lst.append(lst[i]) 
+        i -= 1
+    return new_lst
+
+
+def join_lst(new_lst):
+    result = ''
+    for i in range(len(new_lst)):
+        postfix = ' ' if i != len(new_lst) - 1 else ''
+        result += new_lst[i] + postfix
+    return result
+
 
 def speak_like_jeday(s):
     '''формируем список из строки'''
     lst = get_list(s)
-    return lst
-    # new_lst = revers_list(lst)
-    # result = join_lst(new_lst)
-    # return result
+    new_lst = revers_list(lst)
+    result = join_lst(new_lst)
+    return result
 
-s = 'Я изучаю Питон '
+
+s = 'Я изучаю Питон' # если последний символ не пробел
 print(speak_like_jeday(s), ", мой юный падаван.")
