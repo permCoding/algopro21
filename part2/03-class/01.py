bae55be47341a6c4072a327d87992c0e98015d35
Андрей Беляков <ttxiom@gmail.com>
2022-03-15 14:55:30 +0500

    lecture

--- part2/03-class/01.py
+++ part2/03-class/01.py
@@ -2,12 +2,20 @@ name, age = "Ilon", 51
 person = (name, age)
 
 persons = [
+    person,
     ("Иванов Иван Иванович", 19),
-    ("Ганс Христиан Андерсен", 39),
-    person
+    ("Ганс Христиан Андерсен", 39)
 ]
 
-persons.sort(key=lambda item: item[1])
+def get_par(obj):
+    pass
+    pass
+    return -obj[1]
 
-for person in persons:
+# persons.sort(key=get_par)  # сортирует на месте
+
+sort_persons = sorted(persons, key=lambda obj: obj[1], reverse=True)  # возвращает новый отсортированный список
+
+
+for person in sort_persons:
     print(person[0].ljust(25, ' '), person[1])
--- part2/03-class/02.py
+++ part2/03-class/02.py
@@ -1,7 +1,7 @@
 class Person(object):
     def __init__(self, name, age):
-        self.name = name
         self.age = age
+        self.name = name
 
 
 men1 = Person("Иванов Иван Иванович", 19)
