f7d8a15dad8c3d45c2584b6bd20c1dbd1aa2055c
Андрей Беляков <ttxiom@gmail.com>
2022-03-29 21:28:21 +0500

    up

--- part2/04-list_files/01-subsets/04.py
+++ part2/04-list_files/01-subsets/04.py
@@ -1,1 +1,11 @@
-# очередь
+from itertools import combinations
+
+
+lst = [1, 4, 7]
+
+subsets = []
+for count in range(len(lst)+1):
+    for combo in combinations(lst, count):
+        subsets.append(combo)
+
+print(subsets)
